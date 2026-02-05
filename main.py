33625b20512bd67f20e9ffab0866ff8548d18320
jeffmonribot-hash <jeffmonribot@gmail.com>
2026-02-05 13:53:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 import tkinter as tk
-from pages.accueil import PageAccueil
-
-
-class Application(tk.Tk):
-    def __init__(self):
-        super().__init__()
-
-        # Titre de la fenêtre
-        self.title("Outil de gestion des BDC – CAPB")
-
-        # Taille de la fenêtre (pas trop petite 😉)
-        self.geometry("900x600")
-
-        # Empêche une fenêtre minuscule
-        self.minsize(800, 500)
-
-        # Conteneur principal
-        self.container = tk.Frame(self)
-        self.container.pack(fill="both", expand=True)
-
-        # Affichage de la page d'accueil
-        accueil = PageAccueil(self.container)
-        accueil.pack(fill="both", expand=True)
-
+from app import Application
 
 if __name__ == "__main__":
-    app = Application()
-    app.mainloop()
+    root = tk.Tk()
+    root.title("Outil de gestion des BDC – CAPB")
+    root.geometry("900x600")
+    root.minsize(800, 500)
 
+    app = Application(root)
+    app.pack(fill="both", expand=True)
+
+    root.mainloop()
